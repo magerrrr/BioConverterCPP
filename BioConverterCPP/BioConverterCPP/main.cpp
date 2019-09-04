648e740489f7a49d10f8078e7c022000552c4dbe
Kirill Mager <kiruha24@mail.ru>
2019-09-04 15:20:13 +0300

    The First commit

---
+++ BioConverterCPP/BioConverterCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BioConverterCPP
+//
+//  Created by Kirill on 9/4/19.
+//  Copyright © 2019 Kirill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
